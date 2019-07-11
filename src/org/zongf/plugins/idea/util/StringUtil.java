888fca8815b345de23b1f5387e60ba9139587331
zongf0504 <15737250504@139.com>
2019-07-11 15:07:02 +0800

    新增字符串工具类: 新增字符串截取方法

---
+++ src/org/zongf/plugins/idea/util/StringUtil.java
@@ -0,0 +1,93 @@
+package org.zongf.plugins.idea.util;
+
+/** 新增字符串工具类
+ * @author: zongf
+ * @created: 2019-07-11
+ * @since 1.0
+ */
+public class StringUtil {
+
+    /** 截取第一个seperactor字符串之前的字符串, 如果seperactor不存在, 则返回空字符串
+     * @param str 原字符串
+     * @param seperactor 分隔字符串
+     * @return 空字符串或seperactor之前的字符串
+     * @since 1.0
+     * @author zongf
+     * @created 2019-07-11 
+     */
+    public static String subStringBeforeFirst(String str, String seperactor) {
+        // 如果字符串为空, 则原字符串返回
+        if(str == null || "".equals(str)) return str;
+
+        int idx = str.indexOf(seperactor);
+        return idx == -1 ? "" : str.substring(0, idx);
+    }
+
+    /** 截取最后一个seperactor字符串之前的字符串, 如果seperactor不存在, 则返回空字符串
+     * @param str 原字符串
+     * @param seperactor 分隔字符串
+     * @return 空字符串或seperactor之前的字符串
+     * @since 1.0
+     * @author zongf
+     * @created 2019-07-11
+     */
+    public static String subStringBeforeLast(String str, String seperactor) {
+        // 如果字符串为空, 则原字符串返回
+        if(str == null || "".equals(str)) return str;
+
+        int idx = str.lastIndexOf(seperactor);
+        return idx == -1 ? "" : str.substring(0, idx);
+    }
+
+    /** 截取第一个seperactor字符串之后的字符串, 如果seperactor不存在, 则返回空字符串
+     * @param str 原字符串
+     * @param seperactor 分隔字符串
+     * @return 空字符串或seperactor之前的字符串
+     * @since 1.0
+     * @author zongf
+     * @created 2019-07-11
+     */
+    public static String subStringAfterFirst(String str, String seperactor) {
+
+        // 如果字符串为空, 则原字符串返回
+        if(str == null || "".equals(str)) return str;
+
+        int idx = str.indexOf(seperactor);
+        return idx == -1 ? "" : str.substring(idx + 1);
+    }
+
+    /** 截取最后一个seperactor字符串之后的字符串, 如果seperactor不存在, 则返回空字符串
+     * @param str 原字符串
+     * @param seperactor 分隔字符串
+     * @return 空字符串或seperactor之前的字符串
+     * @since 1.0
+     * @author zongf
+     * @created 2019-07-11
+     */
+    public static String subStringAfterLast(String str, String seperactor) {
+        // 如果字符串为空, 则原字符串返回
+        if(str == null || "".equals(str)) return str;
+
+        int idx = str.lastIndexOf(seperactor);
+        return idx == -1 ? "" : str.substring(idx + 1);
+    }
+
+    /** 截取两个分割符字符串之间的字符串, 如果任意分隔字符串不存在, 则返回空字符串
+     * @param str 原字符串
+     * @param open 开始字符串
+     * @param close 结束字符串
+     * @return 空字符串或seperactor之前的字符串
+     * @since 1.0
+     * @author zongf
+     * @created 2019-07-11
+     */
+    public static String subStringBetween(String str, String open, String close) {
+        // 如果字符串为空, 则返回原字符串
+        if(str == null || "".equals(str)) return str;
+
+        int beginIdx = str.indexOf(open);
+        int endIdx = str.indexOf(close);
+
+        return beginIdx != -1 && endIdx != -1 ? str.substring(beginIdx + 1, endIdx) : "";
+    }
+}
