73ea12ce5cf99b72cb6e4468d472ac652f19ce0a
15737250504@139.com <15737250504@139.com>
2019-08-11 18:11:33 +0800

    修改逻辑为，缓存获取不到，则请求网络

--- src/org/zongf/plugins/idea/cache/MvnVersionResultCache.java
+++ src/org/zongf/plugins/idea/cache/MvnVersionResultCache.java
@@ -1,17 +1,20 @@
 package org.zongf.plugins.idea.cache;
 
+import org.zongf.plugins.idea.util.MvnSearchUtil;
+import org.zongf.plugins.idea.vo.SearchResult;
 import org.zongf.plugins.idea.vo.VersionResult;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * @Description:
- * @author: zongf
- * @date: 2019-08-10 17:26
+/** maven 版本号缓存
+ * @since 1.0
+ * @author zongf
+ * @created 2019-08-11
  */
-public class MvnVersionResultCache implements ICache<String, List<VersionResult>> {
+public class MvnVersionResultCache implements ICache<SearchResult, List<VersionResult>> {
 
     private Map<String,  List<VersionResult>> dataMap = new HashMap<>();
 
@@ -25,23 +28,40 @@ public class MvnVersionResultCache implements ICache<String, List<VersionResult>
     }
 
     @Override
-    public void set(String key,  List<VersionResult> versionResultList) {
+    public void set(SearchResult searchResult,  List<VersionResult> versionResultList) {
+        String key = getKey(searchResult);
         dataMap.put(key, versionResultList);
     }
 
+    /** 尝试从缓存中获取, 如果缓存中没有，则进行查询
+     * @since 1.0
+     * @author zongf
+     * @created 2019-08-11
+     */
     @Override
-    public  List<VersionResult> get(String key) {
-        return dataMap.get(key);
+    public  List<VersionResult> get(SearchResult searchResult) {
+        String key = getKey(searchResult);
+
+        // 尝试从缓存中获取
+        List<VersionResult> resultList = dataMap.get(key);
+
+        // 如果缓存结果为空, 则发送查询
+        if (resultList == null) {
+            resultList = MvnSearchUtil.queryVersions(searchResult);
+            dataMap.put(key, resultList);
+        }
+        return resultList;
     }
 
-    @Override
-    public void clear() {
-        dataMap.clear();
-    }
-
-    @Override
-    public void remove(String key) {
-        dataMap.remove(key);
+    /** 获取缓存中的key
+     * @param searchResult 查询结果
+     * @return String key 格式为 groupId:artifactId
+     * @since 1.0
+     * @author zongf
+     * @created 2019-08-11
+     */
+    private String getKey(SearchResult searchResult) {
+        return searchResult.getGroupId() + ":" + searchResult.getArtifactId();
     }
 
 }
